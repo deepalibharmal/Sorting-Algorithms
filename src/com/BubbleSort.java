6b311efec023c64dc059f6e9ffcd0cabda554033
unknown <dcbharma@ncsu.edu>
2018-10-18 12:49:55 -0400

    V1.1

--- src/com/BubbleSort.java
+++ src/com/BubbleSort.java
@@ -1,10 +1,12 @@
 package com;
-import java.util.*;
 
-public class SelectionSort {
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BubbleSort {
 	
-	public static void main(String args[])
-	{
+public static void main(String args[])
+{
 	long starttime = System.nanoTime();
 	ArrayList<Integer> ar = new ArrayList<Integer>();
 	Scanner sc = new Scanner(System.in);
@@ -15,27 +17,20 @@ public class SelectionSort {
 		ar.add(sc.nextInt());
 	}
 	int temp;
-    for(int i=0;i<n-1;i++)
-    {
-    	for(int j=i+1;j<n;j++)
-    	{
-    		if (ar.get(i) > ar.get(j))
-    		{   temp = ar.get(i);
-    			ar.set(i, ar.get(j));
-    			ar.set(j,temp);
-    			
-    		}
-    			
-    	}
-    	
-    }
-    System.out.println(ar);
+	for(int i=0;i<n-2;i++)
+		for(int j=i+1;j<n-1;j++)
+		{
+			if(ar.get(j)>ar.get(j+1))
+			{
+				temp=ar.get(j);
+				ar.set(j,ar.get(j+1));
+				ar.set(j+1,ar.get(j));
+			}
+				
+				
+		}
+	System.out.println(ar);
 	long endtime = System.nanoTime();
 	System.out.println((endtime-starttime)/1000000000.0);
-	}
-	
-
-
-	
-	
+}
 }
